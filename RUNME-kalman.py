Bug fixes in the quasi-Newton Hessian update.

RUNME-kalman.py
@@ -14,6 +14,8 @@
 # (c) 2015 Johan Dahlin
 # johan.dahlin (at) liu.se
 #
+# Distributed under the MIT license.
+#
 ##############################################################################
 ##############################################################################
 
@@ -72,9 +74,6 @@ km.filter          = km.kf;
 # Use the RTS smoother to estimate the gradient
 km.smoother        = km.rts;
 
-# Set seed to reproduce the results
-np.random.seed( 87655678 );
-
 
 ##############################################################################
 # Setup the PMH algorithm
@@ -104,6 +103,9 @@ ppmh1.invHessian     = np.diag( (3e-02, 1e-03, 2e-03 ) )
 # Preconditioned PMH0 sampler
 ########################################################################
 
+# Set seed to reproduce the results
+np.random.seed( 87655678 );
+
 # Set the step size using the rule of thumb
 ppmh0.stepSize       = 2.562 / np.sqrt(th.nParInference);
 
@@ -121,6 +123,9 @@ iactPPMH0 = ppmh0.calcIACT();
 ########################################################################
 # Preconditioned PMH1 sampler
 ########################################################################
+
+# Set seed to reproduce the results
+np.random.seed( 87655678 );
 
 # Set the step size using the rule of thumb
 ppmh1.stepSize       = 1.125 * np.sqrt( th.nParInference**(-1/3) );
@@ -140,11 +145,14 @@ iactPPMH1 = ppmh1.calcIACT();
 # Quasi-Newton PMH2 sampler with hybrid correction
 ########################################################################
 
+# Set seed to reproduce the results
+np.random.seed( 87655678 );
+
 # Step size the sampler \epsilon_2
 qpmh2.stepSize             = 1.0;
 
 # Set the initial Hessian
-qpmh2.epsilon              = 100;
+qpmh2.epsilon              = 1000;
 
 # Set the memory length of the quasi-Newton proposal
 qpmh2.memoryLength         = 100;
@@ -233,7 +241,7 @@ print("IACT for qPMH2: " + str( np.round(iactQPMH2,0)) + ".");
 
 #IACT for pPMH0: [ 11.  12.  12.].
 #IACT for pPMH1: [ 6.  6.  6.].
-#IACT for qPMH2: [ 5.  8.  4.].
+#IACT for qPMH2: [ 4.  4.  4.].
 
 ########################################################################
 # End of file

RUNME-particles.py
@@ -14,6 +14,8 @@
 # (c) 2015 Johan Dahlin
 # johan.dahlin (at) liu.se
 #
+# Distributed under the MIT license.
+#
 ##############################################################################
 ##############################################################################
 
@@ -153,7 +155,7 @@ iactPPMH1 = ppmh1.calcIACT();
 qpmh2.stepSize             = 1.0;
 
 # Set the initial Hessian
-qpmh2.epsilon              = 100;
+qpmh2.epsilon              = 1000;
 
 # Set the memory length of the quasi-Newton proposal
 qpmh2.memoryLength         = 100;
@@ -242,7 +244,7 @@ print("IACT for qPMH2: " + str( np.round(iactQPMH2,0)) + ".");
 
 #IACT for pPMH0: [ 12.  12.  11.].
 #IACT for pPMH1: [ 6.  6.  8.].
-#IACT for qPMH2: [ 4.  5.  4.].
+#IACT for qPMH2: [ 4.  4.  4.].
 
 ########################################################################
 # End of file

models/models_dists.py
@@ -1,10 +1,19 @@
 ##############################################################################
 ##############################################################################
-# Helpers for distributions and the gradients and Hessian of the log-pdfs
-# 
-# Version 2014-12-12
+# Example code for
+# quasi-Newton particle Metropolis-Hastings
+# for a linear Gaussian state space model
 #
-# Copyright (c) 2014 Johan Dahlin [ johan.dahlin (at) liu.se ]
+# Please cite:
+#
+# J. Dahlin, F. Lindsten, T. B. Sch\"{o}n
+# "Quasi-Newton particle Metropolis-Hastings"
+# Proceedings of the 17th IFAC Symposium on System Identification,
+# Beijing, China, October 2015.
+#
+# (c) 2015 Johan Dahlin
+# johan.dahlin (at) liu.se
+#
 # Distributed under the MIT license.
 #
 ##############################################################################
@@ -25,13 +34,13 @@ def gammaLogPDF(x, a, b):
     # Shape and rate parametrisation
     return a * np.log(b) + (a-1.0) * np.log(x) - b * x - sp.special.gammaln(a)
 
-def gammaLogPDFgradient (x, a, b):    
+def gammaLogPDFgradient (x, a, b):
     # Shape and rate parametrisation
     return ( a - 1.0 ) / x - b;
 
-def gammaLogPDFhessian (x, a, b):    
+def gammaLogPDFhessian (x, a, b):
     # Shape and rate parametrisation
-    return - ( a - 1.0 ) / ( x**2 );    
+    return - ( a - 1.0 ) / ( x**2 );
 
 ##############################################################################
 # Beta pdf, gradient and Hessian of log-pdf
@@ -43,10 +52,10 @@ def betaPDF(x, a, b):
 def betaLogPDF(x, a, b):
     return sp.special.gammaln(a+b) - np.log( sp.special.gamma(a) + sp.special.gamma(b) ) + (a-1.0)*np.log(x) * (b-1.0) * np.log(1.0-x);
 
-def betaLogPDFgradient (x, a, b):    
+def betaLogPDFgradient (x, a, b):
     return ( a - 1.0 ) / x + ( 1.0 - b ) / ( 1.0 - x );
 
-def betaLogPDFhessian (x, a, b):    
+def betaLogPDFhessian (x, a, b):
     return - ( a - 1.0 ) / ( x**2 ) + ( 1.0 - b ) / ( ( 1.0 - x )**2 );
 
 ##############################################################################
@@ -59,10 +68,10 @@ def normalPDF(x, a, b):
 def normalLogPDF(x, a, b):
     return -0.5 * np.log( 2 * np.pi * b**2 ) -0.5/(b**2) * (x-a)**2;
 
-def normalLogPDFgradient (x, a, b):    
+def normalLogPDFgradient (x, a, b):
     return (a - x)/b**2;
 
-def normalLogPDFhessian (x, a, b):    
+def normalLogPDFhessian (x, a, b):
     return -1.0/b**2
 
 ##############################################################################
@@ -85,5 +94,5 @@ def invSigmoidGradient(x):
 ##############################################################################
 ##############################################################################
 # End of file
-##############################################################################   
+##############################################################################
 ##############################################################################

models/models_helpers.py
@@ -1,21 +1,25 @@
-import numpy as np;
+##############################################################################
+##############################################################################
+# Example code for
+# quasi-Newton particle Metropolis-Hastings
+# for a linear Gaussian state space model
+#
+# Please cite:
+#
+# J. Dahlin, F. Lindsten, T. B. Sch\"{o}n
+# "Quasi-Newton particle Metropolis-Hastings"
+# Proceedings of the 17th IFAC Symposium on System Identification,
+# Beijing, China, October 2015.
+#
+# (c) 2015 Johan Dahlin
+# johan.dahlin (at) liu.se
+#
+# Distributed under the MIT license.
+#
+##############################################################################
+##############################################################################
 
-#=============================================================================
-# Make data noisy for the ABC procedure
-#=============================================================================
-def template_makeNoisy(model, sm ):
-    if ( model.transformY == "arctan" ):
-        if sm.weightdist == "boxcar":
-            model.y = np.arctan(model.ynoiseless) + np.random.uniform(-sm.tolLevel,sm.tolLevel,(model.T,1));
-        elif sm.weightdist == "gaussian":
-            model.y = np.arctan(model.ynoiseless) + sm.tolLevel * np.random.randn(model.T,1);
-    elif ( model.transformY == "none" ):
-        if sm.weightdist == "boxcar":
-            model.y = model.ynoiseless + np.random.uniform(-sm.tolLevel,sm.tolLevel,(model.T,1));
-        elif sm.weightdist == "gaussian":
-            model.y = model.ynoiseless + sm.tolLevel * np.random.randn(model.T,1);
-    else:
-        raise NameError("makeNoisy: unknown transformation of data.")
+import numpy as np;
 
 #=============================================================================
 # Copy data from an instance of this struct to another
@@ -152,3 +156,9 @@ def template_generateData(model,u=None,fileName=None,order=None):
             model.y = np.array(tmp[0:model.T,2], copy=True).reshape((model.T,1));
         else:
             raise NameError("generateData, import data: cannot import that order.");
+
+##############################################################################
+##############################################################################
+# End of file
+##############################################################################
+##############################################################################

para/pmh.py
@@ -1,10 +1,19 @@
 ##############################################################################
 ##############################################################################
-# Routines for
-# Particle Metrpolis-Hastings for Bayesian parameter inference
-# Version 2014-12-03
+# Example code for
+# quasi-Newton particle Metropolis-Hastings
+# for a linear Gaussian state space model
 #
-# Copyright (c) 2014 Johan Dahlin [ johan.dahlin (at) liu.se ]
+# Please cite:
+#
+# J. Dahlin, F. Lindsten, T. B. Sch\"{o}n
+# "Quasi-Newton particle Metropolis-Hastings"
+# Proceedings of the 17th IFAC Symposium on System Identification,
+# Beijing, China, October 2015.
+#
+# (c) 2015 Johan Dahlin
+# johan.dahlin (at) liu.se
+#
 # Distributed under the MIT license.
 #
 ##############################################################################
@@ -208,16 +217,18 @@ class stPMH(object):
         # Check if it is PSD
         if ( ~isPSD( self.hessianp [ self.iter,:,: ] ) ):
 
+            eigens = np.linalg.eig(self.hessianp [ self.iter,:,: ])[0];
+
             # Add a diagonal matrix proportional to the largest negative eigv during burnin
             if ( self.iter <= self.nBurnIn ):
                 mineigv = np.min( np.linalg.eig( self.hessianp [ self.iter,:,: ] )[0] )
                 self.hessianp [ self.iter,:,: ] = self.hessianp [ self.iter,:,: ] - 2.0 * mineigv * np.eye( self.nPars )
-                #print("Iteration: " + str(self.iter) + ", mirroring eigenvalues at: " + str( self.thp[self.iter,:] ) + " by adding " +  str( - 2.0 * mineigv ) );
+                print("Iteration: " + str(self.iter) + " has eigenvalues: " + str( eigens ) + " mirroring by adding " +  str( - 2.0 * mineigv ) );
 
             # Replace the Hessian with the posterior covariance matrix after burin
             if ( self.iter > self.nBurnIn ):
                 self.hessianp [ self.iter,:,: ] = self.empHessian;
-                #print("Iteration: " + str(self.iter) + ", replaced Hessian with pre-computed estimated at: " + str( self.thp[self.iter,:] ) );
+                print("Iteration: " + str(self.iter) + " has eigenvalues: " + str( eigens ) + " replaced Hessian with pre-computed estimated." );
 
     ##########################################################################
     # Quasi-Netwon proposal
@@ -230,38 +241,42 @@ class stPMH(object):
         # BFGS update for Hessian estimate
         if ( self.iter > self.memoryLength ):
 
-            # Extract the last unique parameters and their gradients
-            idx = np.sort( np.unique(self.ll,return_index=True)[1] )[-2:];
-
-            if ( np.max( self.iter - idx ) < self.memoryLength ):
-
-                # The last accepted step is inside of the memory length
-                skk = self.th[ idx[1] , : ]       - self.th[ idx[0], : ];
-                ykk = self.gradient[ idx[1] , : ] - self.gradient[ idx[0], : ];
-                foo = np.abs( np.dot( skk, ykk) / np.dot( ykk, ykk) );
-                Hk  = np.eye(self.nPars) * foo;
-
             # Extract estimates of log-likelihood and gradients from the
             # last moves
             self.extractUniqueElements();
 
-            # Add the contribution from the last memoryLength samples
-            for ii in range( (self.thU).shape[0] ):
+            if ( self.nHessianSamples[ self.iter ] > 2 ):
+                # Extract the last unique parameters and their gradients
+                idx = np.sort( np.unique(self.ll,return_index=True)[1] )[-2:];
 
-                # Calculate difference in gradient (ykk) and theta (skk)
-                ykk = self.gradientU[ii,:] - self.gradientU[ii-1,:];
-                skk = self.thU[ii,:]       - self.thU[ii-1,:];
+                if ( np.max( self.iter - idx ) < self.memoryLength ):
 
-                # Check if we have moved, otherwise to not add contribution to Hessian
-                if ( np.sum( skk ) != 0.0 ):
+                    # The last accepted step is inside of the memory length
+                    skk = self.th[ idx[1] , : ]       - self.th[ idx[0], : ];
+                    ykk = self.gradient[ idx[1] , : ] - self.gradient[ idx[0], : ];
+                    foo = np.abs( np.dot( skk, ykk) / np.dot( ykk, ykk) );
+                    Hk  = np.eye(self.nPars) * foo;
 
-                    # Compute rho
-                    rhok = 1.0 / ( np.dot( ykk, skk) );
+                # Add the contribution from the last memoryLength samples
+                for ii in range( (self.thU).shape[0] ):
 
-                    # Update Hessian estimate
-                    A1   = I - skk[:, np.newaxis] * ykk[np.newaxis, :] * rhok
-                    A2   = I - ykk[:, np.newaxis] * skk[np.newaxis, :] * rhok
-                    Hk   = np.dot(A1, np.dot(Hk, A2)) + ( rhok * skk[:, np.newaxis] * skk[np.newaxis, :] )
+                    # Calculate difference in gradient (ykk) and theta (skk)
+                    ykk = self.gradientU[ii,:] - self.gradientU[ii-1,:];
+                    skk = self.thU[ii,:]       - self.thU[ii-1,:];
+
+                    # Check if we have moved, otherwise to not add contribution to Hessian
+                    if ( np.sum( skk ) != 0.0 ):
+
+                        # Compute rho
+                        rhok = 1.0 / ( np.dot( ykk, skk) );
+
+                        # Update Hessian estimate
+                        A1   = I - skk[:, np.newaxis] * ykk[np.newaxis, :] * rhok
+                        A2   = I - ykk[:, np.newaxis] * skk[np.newaxis, :] * rhok
+                        Hk   = np.dot(A1, np.dot(Hk, A2)) + ( rhok * skk[:, np.newaxis] * skk[np.newaxis, :] )
+
+                # Return the negative Hessian estimate
+                Hk = -Hk;
 
         return Hk;
 
@@ -376,6 +391,8 @@ class stPMH(object):
 
         print("writeToFile: wrote results to file: " + fileOutName)
 
-#############################################################################################################################
+##############################################################################
+##############################################################################
 # End of file
-#############################################################################################################################
+##############################################################################
+##############################################################################